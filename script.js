2de7f5de5384fd995d2cb06e4f4b2c99c6c86360
Tejas More <tejasmore660@gmail.com>
2024-05-06 19:08:15 +0530

    created event to move the box when arrow keys are created

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
 const box = document.getElementById("box");
 
+const moveAmount = 12;
+let x = 0;
+let y = 0;
+
+document.addEventListener("keydown", event => {
+
+    event.preventDefault();
+
+    if (event.key.startsWith("Arrow")) {
+
+        switch (event.key) {
+            case "ArrowUp":
+                y -= moveAmount;
+                break;
+
+            case "ArrowDown":
+                y += moveAmount;
+                break;
+
+            case "ArrowLeft":
+                x -= moveAmount;
+                break;
+
+            case "ArrowRight":
+                x += moveAmount;
+                break;
+
+        }
+
+        box.style.top = `${y}px`;
+        box.style.left = `${x}px`;
+    } 
+    
+    else {
+        box.textContent = "🙈";
+        box.style.backgroundColor = "red";
+    }
+
+});
+
 document.addEventListener("keydown", event => {
 
     if (event.key.startsWith("Arrow")) {
